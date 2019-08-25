331660837c75f3d1678933ae0e52c7f3b9b83cfd
丁涛 <54472434+Ding-YuHao@users.noreply.github.com>
2019-08-25 15:02:23 +0900

    for each

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,5 +1,9 @@
 public class Hello {
     public static void main(String[] args) {
-        int[] a = new int[]{1, 2, 3, 4, 5};
+        int[] a = new int[]{3, 4, 5, 6, 7};
+        System.out.println(a[0]);
+        for (int i : a) {
+            System.out.println(i);
+        }
     }
 }
