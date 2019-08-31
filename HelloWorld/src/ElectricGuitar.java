a83eae0a951e99e7b53e2cd916818469a3c4a642
丁涛 <54472434+Ding-YuHao@users.noreply.github.com>
2019-08-31 23:12:58 +0900

    A

---
+++ HelloWorld/src/ElectricGuitar.java
@@ -0,0 +1,24 @@
+public class ElectricGuitar {
+    String brand;
+    int numOfPickups;
+    boolean rockStarusesIt;
+
+    String getBrand() {
+        return brand;
+    }
+    void setBrand(String aBrand) {
+        brand=aBrand;
+    }
+    int getNumOfPickups() {
+        return numOfPickups;
+    }
+    void setNumOfPickups(int num){
+        numOfPickups=num;
+    }
+    boolean getRockStarUseeIt() {
+        return rockStarusesIt;
+    }
+    void setRockStarusesIt(boolean yesOrNo) {
+        rockStarusesIt=yesOrNo;
+    }
+}
--- HelloWorld/src/Test2Drive.java
+++ HelloWorld/src/Test2Drive.java
@@ -1,2 +1,13 @@
 public class Test2Drive {
+    public  static void main (String[] args) {
+        Test2 one=new Test2();
+        one.size=70;
+        Test2 two=new Test2();
+        two.size=8;
+        Test2 three=new Test2();
+        three.size=35;
+        one.bark();
+        two.bark();
+        three.bark();
+    }
 }
